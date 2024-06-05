5c25b8c8184898293b22283632bf3333558e5f8c
MCCSYS <157526855+MACACOSYSTEM@users.noreply.github.com>
2024-06-05 02:48:11 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+const port = 3000;
+
+// hbs
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+// Middlewares
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/js', express.static(__dirname + '/public/js'));
+
+// Endpoints
+app.get('/', (req, res) => {
+    res.render('index', {
+        products: ['banana', 'cebollas', 'lechuga', 'papas', 'pimenton', 'tomate']
+    });
+});
+
+app.listen(port, () => console.log(`Server initialized at port ${port}.`));
